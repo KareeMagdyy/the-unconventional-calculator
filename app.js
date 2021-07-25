37303be08a364a9c9ce72afa6d019fe0d6e0b3e9
KareeMagdyy <karim67899@gmail.com>
2021-07-25 02:52:58 +0200

    adding Calc functions

--- app.js
+++ app.js
@@ -7,8 +7,50 @@ const divideBtn = document.querySelector('#btn-divide');
 const currentResultOutput = document.querySelector('#current-result');
 const currentCalculationOutput = document.querySelector('#current-calculation');
 
+const defaultResult = 0;
+let currentResult = 0;
 
 function outputResult(result, text) {
     currentResultOutput.textContent = result;
     currentCalculationOutput.textContent = text;
 }
+
+function creatAndWriteOutput(operator, resultBefore, resultAfter) {
+    const calcDescription = `${resultBefore} ${operator} ${resultAfter}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const enteredNumber = parseInt(userInput.value);
+    const initResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    creatAndWriteOutput('+', initResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = parseInt(userInput.value);
+    const initResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    creatAndWriteOutput('-', initResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = parseInt(userInput.value);
+    const initResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    creatAndWriteOutput('*', initResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = parseInt(userInput.value);
+    const initResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    creatAndWriteOutput('/', initResult, enteredNumber);
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
